871ba60b5833e7cdd9888871320c1a380dc92178
Mersini Saif <genie.civil.mersini@gmail.com>
2020-12-09 01:28:40 +0100

    adding all

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import Formulaire from './components/Form'
+import Navbar from './components/Navbar'
+import Carousell from './components/Carousel'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App bg-dark">
+      <Navbar/>
+      <Carousell/>
+    <Formulaire/>
+    
+    </div>
+  );
+}
+
+export default App;
